b9f4c3875312ff269ccc98b8ed94525586216387
Vic <305880887@qq.com>
2018-06-08 10:25:52 +0800

    add AlienInvasion example

---
+++ Alien_Invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        self.ship_speed_factor = 1.5
