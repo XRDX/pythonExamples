77e89f0dc0e8133dc7f28d055128e924484b379f
Vic <305880887@qq.com>
2018-06-08 10:32:13 +0800

    add send mail example

---
+++ send_mails/send_mail.py
@@ -0,0 +1,27 @@
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+from_addr = input('From: ') or r"vic.wang@leaplearner.com"
+password = input('Password: ')
+to_addr = input('To: ') or r"vic.wang@leaplearner.com"
+smtp_server = input('SMTP server: ') or r"smtp.mxhichina.com"
+
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+
+msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+msg['Subject'] = Header('This mail is sent be Python', 'utf-8').encode()
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
